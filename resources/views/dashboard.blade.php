7141f4ab3033fae2107d41bc8a4e64360aeda5cd
Edson Famanda <edson@equipmoz.org>
2022-04-06 23:39:33 +0200

    Finalizado a base inicial de projecto

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,11 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
-    </div>
+    <p> CHALLENGE PARA VAGA DE <b>Programador WEB</b> na INFOPEL.</p>
+
+    <br>
+
+    <p> NOME:</p> <b> EDSON JORGE FRANCISCO </b>
+    <p> EMAIL:</p> <b>edsonjorgef1@programmer.net <b>
+    <p> CONTACTO:</p> <b> (+258) 8656333336 </b>
 </x-app-layout>
