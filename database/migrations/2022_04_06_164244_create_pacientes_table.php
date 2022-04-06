149d6ec52152546aa1726342704c13b7a658dae2
Edson Famanda <edson@equipmoz.org>
2022-04-06 19:07:27 +0200

    first commit

---
+++ database/migrations/2022_04_06_164244_create_pacientes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nome');
+            $table->datetime('Data_de_nascimento');
+            $table->string('sexo');
+            $table->string('Profissao')->nullable();
+            $table->string('Endereço');
+            $table->string('Seguro')->nullable();
+            $table->string('Contacto')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
