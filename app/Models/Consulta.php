ea2620cdc046a77d0f0af9cc9544365256952499
Edson Famanda <edson@equipmoz.org>
2022-04-06 19:22:45 +0200

    Preenchimento de fillable - Consulta && Relacionamento de ID

--- app/Models/Consulta.php
+++ app/Models/Consulta.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Consulta extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sector_medico',
+        'data_entrada',
+        'data_saida',
+        'horario_entrada',
+        'status',
+        'observacao',
+        'paciente_id',
+    ];
+
+    public function paciente(){
+        return $this->belongsTo(Paciente::class);
+    }
 }
