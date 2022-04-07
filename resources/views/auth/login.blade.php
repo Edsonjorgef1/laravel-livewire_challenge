5a5bea91eef30d01e7450772696962c2a1cd2785
Edson Famanda <edson@equipmoz.org>
2022-04-07 14:34:03 +0200

    Botao de Registrar-se na pagina de Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,11 +33,15 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                @endif
+
+                {{-- @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
-                @endif
+                @endif --}}
 
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
