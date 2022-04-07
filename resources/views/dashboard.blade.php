49538fa767d55c3b9c0cc5b64ca96893c8f02685
Edson Famanda <edson@equipmoz.org>
2022-04-07 09:24:30 +0200

    Actualizacoes de Dados

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,12 @@
         </h2>
     </x-slot>
 
-    <p> CHALLENGE PARA VAGA DE <b>Programador WEB</b> na INFOPEL.</p>
-
+    <p> CHALLENGE <b>Programador WEB</b>.</p>
     <br>
 
-    <p> NOME:</p> <b> EDSON JORGE FRANCISCO </b>
-    <p> EMAIL:</p> <b>edsonjorgef1@programmer.net <b>
-    <p> CONTACTO:</p> <b> (+258) 8656333336 </b>
+    <p>NOME: <b> EDSON JORGE FRANCISCO </b></p>
+    <p>EMAIL: <b> <a href="mailto:edsonjorgef1@gmail.com">edsonjorgef1@gmail.com</a> / <a href="mailto:edsonjorgef1@programmer.net">edsonjorgef1@programmer.net</a> </b></p>
+    <p>LINKEDIN: <b> <a target="_blank" href="https://www.linkedin.com/in/edsonjorgef1">https://www.linkedin.com/in/edsonjorgef1</a></b></p>
+    <p>GITHUB:  <b> <a target="_blank" href="https://github.com/Edsonjorgef1/laravel-livewire_challenge">https://github.com/Edsonjorgef1/laravel-livewire_challenge</a></b></p>
+    <p>CONTACTO: <b> (+258) 8656333336 / 8456333335 </b></p>
 </x-app-layout>
